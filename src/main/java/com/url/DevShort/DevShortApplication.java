f9c7fc71d56da513e6d0a84f15247d840b0d6291
sh78ba <sh78ba@gmail.com>
2025-06-01 01:43:01 +0530

    first commit

---
+++ src/main/java/com/url/DevShort/DevShortApplication.java
@@ -0,0 +1,13 @@
+package com.url.DevShort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevShortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevShortApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/url/DevShort/DevShortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.url.DevShort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevShortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
