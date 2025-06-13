a0e5d2199ddb9e45999c151462c4471b5be23f86
sh78ba <sh78ba@gmail.com>
2025-06-13 22:57:25 +0530

    completed registerr

--- src/main/java/com/url/DevShort/controller/AuthController.java
+++ src/main/java/com/url/DevShort/controller/AuthController.java
@@ -22,6 +22,7 @@ public class AuthController {
     public ResponseEntity<?>registerUser(@RequestBody RegisterRequest registerRequest){
     User user=new User();
     user.setUsername(registerRequest.getUsername());
+    user.setEmail(registerRequest.getEmail());
     user.setPassword(registerRequest.getPassword());
     user.setRole("ROLE_USER");
 
